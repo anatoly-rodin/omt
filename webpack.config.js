43a87bdca3c6fb91ad4f902f89a0009b4b932c6b
Anatoly Rodin <anatolyr1@gmail.com>
2018-07-24 16:35:29 +0300

    Правки. Финал.

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,31 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: ['babel-polyfill', './src/main.js'],
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		publicPath: '/dist/',
-		filename: 'build.js'
-	},
-	module: {
-		rules: [
-			// JS
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env'],
-						plugins: ['transform-runtime']
-					}
-				}
-			},
-			// CSS
-			{
-				test: /\.(scss|css)$/,
-				use: [
-				    {
+    entry: ['babel-polyfill', './src/main.js'],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            },
+            // CSS
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
                         loader: 'style-loader'
                     },
                     {
@@ -38,12 +38,22 @@ module.exports = {
                             includePaths: ["absolute/path/a", "absolute/path/b"]
                         }
                     }
-					
-				]
-			},
-			// Images
+                    
+                ]
+            },
+            // Images
             {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                test: /\.(jpeg|png)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[name].[ext]',
+                    },
+                },
+            },
+            // Images
+            {
+                test: /\.(svg)(\?.*)?$/,
                 use: {
                     loader: 'svg-url-loader',
                     options: {
@@ -51,43 +61,43 @@ module.exports = {
                     },
                 },
             },
-			// Fonts
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]'
-				}
-			}
-		]
-	},
-	resolve: {
-		alias: {
-			'vue$': 'vue/dist/vue.esm.js'
-		}
-	},
-	devServer: {
-		historyApiFallback: true,
-		noInfo: true
-	},
-	performance: {
-		hints: false
-	},
-	devtool: '#eval-source-map',
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"development"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true
-		})
-	]
+            // Fonts
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"development"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ]
 };
